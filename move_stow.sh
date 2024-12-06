9c1f76f702a997aa7b48b33df411fcff49fcf69d
rushdynamic <tameimpalafanclubpresident@gmail.com>
2024-12-07 01:48:52 +0530

    feat: configs for ranger and rofi

--- move_stow.sh
+++ move_stow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-push_config() {
+move_stow() {
     local prog_name="$1"
     local curr_path="/home/rushdynamic/.config/$1"
     local new_path="./$1/.config/"
@@ -11,9 +11,9 @@ push_config() {
 
 if [ -z "$1" ]; then
     echo "Usage: $0 <program-name>"
-    echo -e "Example:\n `$0 rofi`\n"
-    echo "This would copy `~/.config/rofi` to `./rofi/config/rofi`"
+    echo -e "Example:\n \`$0 rofi\`"
+    echo -e "This would copy \`~/.config/rofi\` to \`./rofi/config/rofi\`"
     exit 1
 fi
 
-push_config $1
+move_stow $1
