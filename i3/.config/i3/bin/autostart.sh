d483ed66b9a5a755124eff559d490adda603607c
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-02-05 01:39:52 +0530

    feat: polybar customization

--- i3/.config/i3/bin/autostart.sh
+++ i3/.config/i3/bin/autostart.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 picom --config ~/.config/picom/picom.conf &
 feh --bg-fill /home/rushdynamic/Pictures/Wallpapers/1356532.png &
-polybar &
+polybar -c ~/.config/polybar/personal-config &
