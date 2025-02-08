f159751922b378e2c622825bf85642a7b764f7a7
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-02-08 16:35:18 +0530

    feat: add virtual mic monitor device to pulseaudio

--- i3/.config/i3/bin/autostart.sh
+++ i3/.config/i3/bin/autostart.sh
@@ -3,3 +3,4 @@ picom --config ~/.config/picom/picom.conf &
 feh --bg-fill /home/rushdynamic/Pictures/Wallpapers/1356532.png &
 polybar -c ~/.config/polybar/personal-config &
 autotiling -w 2 &
+pactl load-module module-null-sink sink_name=virtmic sink_properties=device.description=Virtual_Microphone_Sink rate=48000 channels=2
