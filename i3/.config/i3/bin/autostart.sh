2e8adcbbf2e3e1bb914b5207850d098fd299c600
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-02-15 16:59:16 +0530

    feat: randomize feh wallpapers

--- i3/.config/i3/bin/autostart.sh
+++ i3/.config/i3/bin/autostart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 picom --config ~/.config/picom/picom.conf &
-feh --bg-fill /home/rushdynamic/Pictures/Wallpapers/1356532.png &
+feh --bg-fill --randomize --no-fehbg /home/rushdynamic/Pictures/Wallpapers/ &
 polybar -c ~/.config/polybar/config &
 autotiling -w 2 &
 pactl load-module module-null-sink sink_name=virtmic sink_properties=device.description=Virtual_Microphone_Sink rate=48000 channels=2
