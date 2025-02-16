6b5b474a786f3883cf977ae2eae8829d931c0eff
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-02-16 18:51:31 +0530

    feat: improve shadows and borders

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -254,6 +254,8 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  virtualisation.virtualbox.host.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
