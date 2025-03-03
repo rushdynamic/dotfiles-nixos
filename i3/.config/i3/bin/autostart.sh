1cb1c1fe7f688856af13e0c2fa40fd88ec9e43c7
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-03-04 02:21:46 +0530

    feat: i3 autotiling for all primary workspaces

--- i3/.config/i3/bin/autostart.sh
+++ i3/.config/i3/bin/autostart.sh
@@ -2,5 +2,5 @@
 picom --config ~/.config/picom/picom.conf &
 feh --bg-fill --randomize --no-fehbg /home/rushdynamic/Pictures/Wallpapers/ &
 polybar -c ~/.config/polybar/config &
-autotiling -w 2 &
+autotiling -w 1 2 3 4 &
 pactl load-module module-null-sink sink_name=virtmic sink_properties=device.description=Virtual_Microphone_Sink rate=48000 channels=2
