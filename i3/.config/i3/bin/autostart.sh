7c11a3b1df6ed86ff9ca88d293b31cd50c039169
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-03-28 00:30:50 +0530

    fix: polybar and i3 dual-monitor setup

--- i3/.config/i3/bin/autostart.sh
+++ i3/.config/i3/bin/autostart.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 picom --config ~/.config/picom/picom.conf &
 feh --bg-fill --randomize --no-fehbg /home/rushdynamic/Pictures/Wallpapers/ &
-polybar -c ~/.config/polybar/config &
+xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal &
+polybar --reload laptop -c ~/.config/polybar/config &
+polybar --reload external -c ~/.config/polybar/config &
 autotiling -w 1 2 3 4 &
 pactl load-module module-null-sink sink_name=virtmic sink_properties=device.description=Virtual_Microphone_Sink rate=48000 channels=2
