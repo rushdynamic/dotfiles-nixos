8b3088a0d2da5de2b8a2681bd4aaa4715fe75e69
rushdynamic <tameimpalafanclubpresident@gmail.com>
2026-01-24 01:16:26 +0530

    fix(i3): autostart services log paths

--- i3/bin/autostart.sh
+++ i3/bin/autostart.sh
@@ -43,8 +43,8 @@ fi
 feh --bg-fill --no-fehbg $wallpaper_path &
 
 if ! pgrep -f "bin/redshift" > /dev/null; then
-	nohup redshift > redshift-output.log 2>&1 &
+	nohup redshift > /var/log/redshift-output.log 2>&1 &
 fi
-nohup caffeine > caffeine-output.log 2>&1 &
-nohup stretchly > stretchly-output.log 2>&1 &
-nohup flameshot > flameshot-output.log 2>&1 &
+nohup caffeine > /var/log/caffeine-output.log 2>&1 &
+nohup stretchly > /var/log/stretchly-output.log 2>&1 &
+nohup flameshot > /var/log/flameshot-output.log 2>&1 &
